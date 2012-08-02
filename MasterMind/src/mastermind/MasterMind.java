c494fd1e7c2d7767bf83109cdad86d9dce5773a8
katri <katri@ville-AOA110.(none)>
2012-08-03 00:13:53 +0300

    eka commit

---
+++ MasterMind/src/mastermind/MasterMind.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mastermind;
+
+/**
+ *
+ * @author katri
+ */
+public class MasterMind {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
